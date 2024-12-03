0e09237bd20aef452dd47c01ce4606bc8bea7751
tarek habash <tarek.soliman797@gmail.com>
2024-12-03 02:33:47 +0100

    Delete product by id

--- src/main/java/com/example/sbj/product/services/DeleteProductService.java
+++ src/main/java/com/example/sbj/product/services/DeleteProductService.java
@@ -1,15 +1,31 @@
 package com.example.sbj.product.services;
 
 import com.example.sbj.Command;
+import com.example.sbj.product.ProductRepository;
+import com.example.sbj.product.model.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class DeleteProductService implements Command<Void, String> {
+public class DeleteProductService implements Command<Integer, Void> {
+
+    private ProductRepository productRepository;
+
+    public DeleteProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     @Override
-    public ResponseEntity<String> execute(Void input) {
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Product Deleted");
+    public ResponseEntity<Void> execute(Integer id) {
+        Optional<Product> product = productRepository.findById(id);
+
+        if (product.isPresent()) {
+            productRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return null;
     }
 }

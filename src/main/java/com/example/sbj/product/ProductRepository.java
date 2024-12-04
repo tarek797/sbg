7a575cbb0c55a91b7088e8f81324616838967b0b
tarek habash <tarek.soliman797@gmail.com>
2024-12-04 04:46:02 +0100

    Search Product By name or description with custom query

--- src/main/java/com/example/sbj/product/ProductRepository.java
+++ src/main/java/com/example/sbj/product/ProductRepository.java
@@ -2,6 +2,8 @@ package com.example.sbj.product;
 
 import com.example.sbj.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,7 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     List<Product> findByNameContaining(String name);
+
+    @Query("SELECT p FROM Product p WHERE p.name LIKE %:keyword% OR p.description LIKE %:keyword%")
+    List<Product> findByNameOrDescriptionContaining(@Param("keyword") String keyword);
 }
--- src/main/java/com/example/sbj/product/services/SearchProductService.java
+++ src/main/java/com/example/sbj/product/services/SearchProductService.java
@@ -18,7 +18,7 @@ public class SearchProductService implements Query<String, List<ProductDTO>> {
 
     @Override
     public ResponseEntity<List<ProductDTO>> execute(String name) {
-        return ResponseEntity.ok(productRepository.findByNameContaining(name)
+        return ResponseEntity.ok(productRepository.findByNameOrDescriptionContaining(name)
                 .stream()
                 .map(ProductDTO::new)
                 .toList());

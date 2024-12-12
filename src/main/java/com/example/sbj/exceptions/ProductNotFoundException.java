f6bcb09928b8306e392217f4420610f4ee70b4c5
tarek habash <tarek.soliman797@gmail.com>
2024-12-12 04:51:53 +0100

    Add Logging statements

--- src/main/java/com/example/sbj/exceptions/ProductNotFoundException.java
+++ src/main/java/com/example/sbj/exceptions/ProductNotFoundException.java
@@ -1,11 +1,15 @@
 package com.example.sbj.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProductNotFoundException extends RuntimeException {
+    private static final Logger logger = LoggerFactory.getLogger(ProductNotFoundException.class);
     public ProductNotFoundException() {
         super(ErrorMessages.PRODUCT_NOT_FOUND.getMessage());
+        logger.error("Exception {} thrown ", getClass());
     }
 }
--- src/main/java/com/example/sbj/product/services/GetProductService.java
+++ src/main/java/com/example/sbj/product/services/GetProductService.java
@@ -4,6 +4,8 @@ import com.example.sbj.Query;
 import com.example.sbj.product.ProductRepository;
 import com.example.sbj.product.model.Product;
 import com.example.sbj.product.model.ProductDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +16,15 @@ public class GetProductService implements Query<Integer, ProductDTO> {
 
     private final ProductRepository productRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(GetProductsService.class);
+
     public GetProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     @Override
     public ResponseEntity<ProductDTO> execute(Integer input) {
+        logger.info("Excuting " + getClass() + " input " + input);
         Optional<Product> productOptional = productRepository.findById(input);
         if (productOptional.isPresent()) {
             return ResponseEntity.ok(new ProductDTO(productOptional.get()));
